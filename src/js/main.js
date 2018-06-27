464969ebff16031373208a610050e4ed46986c2c
Vlad Tepes <vg.email.2@gmail.com>
2018-06-27 18:57:19 +0300

    styled swiper added

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,16 @@ $(document).ready(function () {
     //----------------------<<svg for ie>>----------------------\\
     svg4everybody();
 
+    const mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-nx',
+            prevEl: '.swiper-button-pr'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    })
 });
