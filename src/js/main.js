549dbca960bf2442d0228bba5c3db4b318251e86
Vlad Tepes <vg.email.2@gmail.com>
2018-06-29 14:33:21 +0300

    styled map added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,43 @@
+//----------------------<<google map>>----------------------\\
+function initMap() {
+    const myLatLng = {lat: 40.798054, lng: -74.064936};
+
+    const style = [
+        {
+          featureType: "all",
+          elementType: "all",
+          stylers: [
+            { saturation: -100 }
+          ]
+        }
+    ];
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 16,
+        disableDefaultUI: true,
+        zoomControl: true,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'tehgrey']
+       }
+    });
+
+    const mapType = new google.maps.StyledMapType(style, { name:"Greyscale" });
+          map.mapTypes.set('tehgrey', mapType);
+          map.setMapTypeId('tehgrey');
+
+    const marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: "Dublin's iPad Apps",
+        icon: "assets/img/svg/marker.svg"
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
+
 $(document).ready(function () {
 
     //----------------------<<svg for ie>>----------------------\\
